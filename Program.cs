65eea7d6aa8d7b35afe5873eaa5bc5ca86cdef6f
Elena <elena1709@yandex.ru>
2022-10-18 11:21:24 +0300

    commit for hw

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте массив заполненный случайными
+// положительными трёхзначными числами. Напишите
+// программу, которая покажет количество чётных чисел в
+// массиве
+
+int size = new Random().Next(1, 10);
+int[] array = new int[size];
+int count = 0;
+for (int i = 0; i < size; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+    if (array[i] % 2 == 0) count++;
+}
+Console.Clear();
+Console.WriteLine($"В данном массиве: [{String.Join(", ", array)}]\n"
++ $"Содержится четных чисел: {count}");
